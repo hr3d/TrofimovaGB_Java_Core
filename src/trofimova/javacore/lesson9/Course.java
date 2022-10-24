e414fd47a946c55b8c726ee6735500b47f5bb686
Trofimova Elena <hr3d@yandex.ru>
2022-10-24 12:45:08 +0300

    Added files to lesson9

---
+++ src/trofimova/javacore/lesson9/Course.java
@@ -0,0 +1,14 @@
+package trofimova.javacore.lesson9;
+
+public class Course {
+
+    private String courseName;
+
+    public Course(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+}
---
+++ src/trofimova/javacore/lesson9/Student.java
@@ -0,0 +1,22 @@
+package trofimova.javacore.lesson9;
+
+import java.util.List;
+
+public class Student {
+
+    private String name;
+    private List<Course> courses;
+
+    public Student(String name, List<Course> courses) {
+        this.name = name;
+        this.courses = courses;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+}
